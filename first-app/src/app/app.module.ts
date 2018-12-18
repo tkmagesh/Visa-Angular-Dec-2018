c35a3232a6881596593b725d59b4405adcf549ab
Magesh <tkmagesh77@gmail.com>
2018-12-18 12:16:00 +0800

    Component composition using the CalculatorResult component

--- first-app/src/app/app.module.ts
+++ first-app/src/app/app.module.ts
@@ -5,12 +5,15 @@ import { AppComponent } from './app.component';
 import { GreeterComponent } from './greeter/greeter.component';
 import { CalculatorComponent } from './calculator/calculator.component';
 import { CalculatorTwoComponent } from './calculator/calculatorTwo.component';
+import { CalculatorResultComponent } from './calculator/calculatorResult.component';
+
 @NgModule({
   declarations: [
     AppComponent
     , GreeterComponent
     , CalculatorComponent
     , CalculatorTwoComponent
+    , CalculatorResultComponent
   ],
   imports: [
     BrowserModule
