bc5d4ee9c4fa6dc5788bee25e38060e010fb1204
Magesh <tkmagesh77@gmail.com>
2018-12-18 11:25:34 +0800

    Calculator - solution

--- first-app/src/app/app.module.ts
+++ first-app/src/app/app.module.ts
@@ -4,12 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { GreeterComponent } from './greeter/greeter.component';
 import { CalculatorComponent } from './calculator/calculator.component';
-
+import { CalculatorTwoComponent } from './calculator/calculatorTwo.component';
 @NgModule({
   declarations: [
     AppComponent
     , GreeterComponent
     , CalculatorComponent
+    , CalculatorTwoComponent
   ],
   imports: [
     BrowserModule
